3d37b325e5d7027e1747dc982e90165b73a46591
Demétrius Soares do Nascimento <demetriussoaresnascimento555@gmail.com>
2024-02-04 16:27:20 -0300

    Aula 10

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -1,11 +1,13 @@
 /// <reference types="cypress" />
 
 describe('Cypress basics', () => {
-    it('Should visit a page and assert title', () => {
+    it.only('Should visit a page and assert title', () => {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
 
         // const title = cy.title()
         // console.log(title)
+
+        cy.pause()
 
         cy.title().should('be.equal', 'Campo de Treinamento')
         cy.title().should('contain', 'Campo')
@@ -18,7 +20,7 @@ describe('Cypress basics', () => {
         //TODO escrever o log em um campo de texto
     })
 
-    it.only('Should find and interact with an element', () => {
+    it('Should find and interact with an element', () => {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
 
         // cy.get('nao existe')
