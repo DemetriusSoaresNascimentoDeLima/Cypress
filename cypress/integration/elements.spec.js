e8cf338152c32764c0d8219fd5dc4584a12d9619
Demétrius Soares do Nascimento <demetriussoaresnascimento555@gmail.com>
2024-02-07 11:25:20 -0300

    Aula 15

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -29,7 +29,7 @@ describe('Work with basic elements', () => {
         cy.get('#resultado').should('have.text', 'Voltou!')
     })
 
-    it.only('TextFields', () => {
+    it('TextFields', () => {
         cy.get('#formNome').type('Cypress Test')
         cy.get('#formNome').should('have.value', 'Cypress Test')
 
@@ -50,4 +50,17 @@ describe('Work with basic elements', () => {
             .should('have.value', 'acerto')
 
     })
+
+    it('Radio Button', () => {
+        cy.get('#formSexoFem')
+            .click()
+            .should('be.checked')
+
+        cy.get('#formSexoMasc')
+            .should('not.be.checked')
+
+            cy.get("[name='formSexo']")
+                .should('have.length', 2)
+    })
+
 })
