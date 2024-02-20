0e92f1add8d11489bb46772b7313e8db047f8cfa
Demétrius Soares do Nascimento <demetriussoaresnascimento555@gmail.com>
2024-02-20 16:04:43 -0300

    Aula 40

--- cypress/integration/popup.spec.js
+++ cypress/integration/popup.spec.js
@@ -10,7 +10,7 @@ describe('Work with Popup', () => {
     
     })
 
-    it.only('Deve verificar se o popup foi invocado', () => {
+    it('Deve verificar se o popup foi invocado', () => {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
         cy.window().then(win => {
             cy.stub(win, 'open').as('winOpen')
@@ -18,6 +18,33 @@ describe('Work with Popup', () => {
         cy.get('#buttonPopUp').click()
         cy.get('@winOpen').should('be.called')
 
+    })
+
+    describe.only('With links', () => {
+        beforeEach(() => {
+            cy.visit('https://wcaquino.me/cypress/componentes.html')
+        })
+
+        it('Check popup url', () => {
+            cy.contains('Popup2')
+                .should('have.prop', 'href')
+                .and('equal', 'https://wcaquino.me/cypress/frame.html')
+        })
+
+        it('Should access popup dinamically', () => {
+            cy.contains('Popup2').then($a => {
+                const href = $a.prop('href')
+                cy.visit(href)
+                cy.get('#tfield').type('funciona')
+            })
+        })
+
+        it('Should force link on same page', () => {
+            cy.contains('Popup2')
+                .invoke('removeAttr', 'target')
+                .click()
+            cy.get('#tfield').type('funciona')
+        })
     })
 
 })
