d5b876757e0d12772cbca85cf54a2cc79f9b033b
Demétrius Soares do Nascimento <demetriussoaresnascimento555@gmail.com>
2024-02-20 15:31:50 -0300

    Aula 37

---
+++ cypress/integration/iframe.spec.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+
+describe('Work with iFrames', () => {
+    it('Deve preencher campo de texto', () => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+        cy.get('#frame1').then(iframe => {
+            const body = iframe.contents().find('body')
+            cy.wrap(body).find('#tfield')
+                .type('funciona?')
+                .should('have.valeu', 'funciona?')
+
+            cy.on('window:alert', msg => {
+                expect(msg).to.be.equal('Alert Simples')
+            })
+            cy.wrap(body).find('#otherButton').click()
+        })
+
+    })
+})
