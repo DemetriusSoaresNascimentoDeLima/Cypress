2d1e149c004a0daacc7601a01f1e93d2773fac33
Demétrius Soares do Nascimento <demetriussoaresnascimento555@gmail.com>
2024-02-29 23:08:43 -0300

    Aula 57

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -1,29 +1,22 @@
 /// <reference types="cypress" />
 
 import loc from '../../support/locators'
+import '../../support/commandsContas'
 
 describe('Should test at a function level', () => {
     before(() => {
         cy.login('test@cypress', '123456')
         cy.resetApp()
-        // cy.visit('https://barrigareact.wcaquino.me/')
-        // cy.get(loc.LOGIN.USER).type('test@cypress')
-        // cy.get(loc.LOGIN.PASSWORD).type('123456')
-        // cy.get(loc.LOGIN.BTN_LOGIN).click()
-        // cy.get(loc.MESSAGE).should('contain', 'Bem vindo')
     })
 
     it('Should create an account', () => {
-        cy.get(loc.MENU.SETTINGS).click()
-        cy.get(loc.MENU.CONTAS).click()
-        cy.get(loc.CONTAS.NOME).type('Conta de teste')
-        cy.get(loc.CONTAS.BTN_SALVAR).click()
+        cy.acessarMenuConta()
+        cy.inserirConta('Conta de teste')
         cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso!')
     })
 
     it('Should update an account', () => {
-        cy.get(loc.MENU.SETTINGS).click()
-        cy.get(loc.MENU.CONTAS).click()
+        cy.acessarMenuConta()
         cy.xpath(loc.CONTAS.XP_BTN_ALTERAR).click()
         cy.get(loc.CONTAS.NOME)
             .clear()
